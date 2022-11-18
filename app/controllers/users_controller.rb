d8b9b7f7c177e85cc9dae7d6943f6030e0328129
MisterCxmpy <cosgunhussein@gmail.com>
2022-11-18 12:39:05 +0000

    Create friends list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def my_portfolio
     @tracked_stocks = current_user.stocks
   end
+
+  def my_friends
+    @friends = current_user.friends
+  end
 end
