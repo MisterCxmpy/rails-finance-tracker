3e1ba49f4a01cb88e26374b3445458fc1ef8fb39
MisterCxmpy <cosgunhussein@gmail.com>
2022-11-18 19:58:59 +0000

    Display user/friend profile and tracked stocks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def my_portfolio
+    @user = current_user
     @tracked_stocks = current_user.stocks
   end
 
@@ -27,5 +28,10 @@ class UsersController < ApplicationController
         format.js { render partial: 'users/friend_result' }
       end
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
   end
 end
