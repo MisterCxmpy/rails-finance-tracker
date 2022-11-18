524a64f37b0c3d249e9fdc63b9c5e2c8b85a46fa
MisterCxmpy <cosgunhussein@gmail.com>
2022-11-18 15:14:58 +0000

    Implement search users/friends search methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,18 +12,18 @@ class UsersController < ApplicationController
       @friend = params[:friend]
       if @friend
         respond_to do |format|
-          format.js { render partial: 'users/friend_results' }
+          format.js { render partial: 'users/friend_result' }
         end
       else
         respond_to do |format|
           flash.now[:alert] = "Couldn't find user"
-          format.js { render partial: 'users/friend_results' }
+          format.js { render partial: 'users/friend_result' }
         end
       end    
     else
       respond_to do |format|
         flash.now[:alert] = "Please enter a friend name or email to search"
-        format.js { render partial: 'users/friend_results' }
+        format.js { render partial: 'users/friend_result' }
       end
     end
   end
