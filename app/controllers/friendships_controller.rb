559d3cb242e854a4d96d0fae8d682159c78b9e37
MisterCxmpy <cosgunhussein@gmail.com>
2022-11-18 17:19:51 +0000

    Implement add friend

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,11 +1,21 @@
 class FriendshipsController < ApplicationController
   def create
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "You are now following #{friend.full_name}"
+    else
+      flash[:alert] = "Something went wrong"
+    end
+    redirect_to my_friends_path
   end
 
   def destroy
     friendship = current_user.friendships.where(friend_id: params[:id]).first
-    flash[:notice] = "Stopped following #{friendship.first_name}"
+    friend = User.find(params[:id])
+    flash[:notice] = "Stopped following #{friend.full_name}"
     friendship.destroy
+    friend.destroy
     redirect_to my_friends_path
   end
 end
